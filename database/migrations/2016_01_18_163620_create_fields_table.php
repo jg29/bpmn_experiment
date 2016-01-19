2cd812e28ae7fcfc93f14159e2119b0bfd385022
jg29 <jonas.gutermuth@uni-ulm.de>
2016-01-19 22:09:06 +0100

    neue Datenbankfiles

---
+++ database/migrations/2016_01_18_163620_create_fields_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFieldsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fields', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('type');
+            $table->string('name');
+            $table->string('validation');
+            $table->text('settings');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('fields');
+    }
+}
