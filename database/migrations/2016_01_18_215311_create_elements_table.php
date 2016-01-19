96b481458c80709d8b88323937ec80741382e2ad
jg29 <jonas.gutermuth@uni-ulm.de>
2016-01-19 22:09:23 +0100

    neue Datenbankfiles

---
+++ database/migrations/2016_01_18_215311_create_elements_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateElementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('elements', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('type');
+            $table->string('title');
+            $table->text('Content');
+            $table->integer('ref');
+            $table->integer('next');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('elements');
+    }
+}
---
+++ database/migrations/2016_01_18_215342_create_experiments_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExperimentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('experiments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('editor');
+            $table->integer('group');
+            $table->string('title');
+            $table->text('text');
+            $table->integer('element');
+            $table->string('key');
+            $table->timestamps();
+
+            $table->foreign('editor')
+                ->reference('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('experiments');
+    }
+}
